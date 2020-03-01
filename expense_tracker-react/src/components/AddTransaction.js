885b2de198394afdaec64391053dc2f023ca98bf
Thiago Vieira <th91v@outlook.com>
2020-03-01 12:55:11 -0300

    actions de adicionar e apagar transações

--- expense_tracker-react/src/components/AddTransaction.js
+++ expense_tracker-react/src/components/AddTransaction.js
@@ -1,13 +1,28 @@
-import React, { useState} from 'react'
+import React, { useState, useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
 
+    const { addTransaction } = useContext(GlobalContext);
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        const newTransaction = {
+            id:  Math.floor(Math.random() * 1000000000), // susbtituir esta lógica por um RANGE
+            text, //text: text
+            amount: +amount // sinal de + foi inserido para trabsformar o valor em um Number, senão receberiamos um erro 'TypeError: amounts.reduce(...).toFixed is not a function'
+        }
+
+        addTransaction(newTransaction);
+    }
+
     return (
         <>
             <h3>Add new transaction</h3>
-            <form >
+            <form onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Expense title</label>
                     <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter transaction title..."></input>
--- expense_tracker-react/src/context/AppReducer.js
+++ expense_tracker-react/src/context/AppReducer.js
@@ -1,6 +1,17 @@
-// especifica as mudanças de estados da aplicação em respostas a mudnaças no contexto
+// especifica as mudanças de estados da aplicação em respostas a mudanças no contexto e os repassa para os componentes
+// por causa do paradigma funcional, do react, na verdade AppReducer cria um novo estado e o repassa
 export default (state, action) => {
     switch (action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [action.payload, ...state.transactions]
+            }
         default:
             return state;
     }
